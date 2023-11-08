5cef40bbefa03ccdeb1faed3d598cb9136e582f3
divyansh <bt22cse003@iiitn.ac.in>
2023-11-08 14:51:59 +0530

    Initial Commit

---
+++ index2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int printfunc()
+{
+    int i;
+    while(i<=20)
+    {
+        cout<<i<<"";
+        i++;
+    }
+    return 0;
+}
+int main()
+{
+    printfunc();
+    return 0;
+}
